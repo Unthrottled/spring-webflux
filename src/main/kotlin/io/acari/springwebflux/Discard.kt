384d3552c6db63a12bfe30b74f505bea4478ec7c
DATBOI <alexsimons9999@gmail.com>
2018-07-19 17:43:24 -0500

    Derp search/replace fuckup

--- src/main/kotlin/io/acari/springwebflux/Discard.kt
+++ src/main/kotlin/io/acari/springwebflux/Discard.kt
@@ -1,8 +1,15 @@
 package io.acari.springwebflux
 
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
-import org.bson.ByteBuf
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.util.ReferenceCountUtil
+import io.netty.channel.socket.SocketChannel
 
 /**
  * Forged in the flames of battle by alex.
@@ -11,11 +18,46 @@ import org.bson.ByteBuf
 class DiscardServerHandler : ChannelInboundHandlerAdapter() {
     override fun channelRead(ctx: ChannelHandlerContext?, msg: Any?) {
         val message = msg as ByteBuf
-        message.release()
+        try {
+
+        } finally {
+            ReferenceCountUtil.release(msg)
+        }
     }
 
     override fun exceptionCaught(ctx: ChannelHandlerContext?, cause: Throwable?) {
         cause?.printStackTrace()
         ctx?.close()
     }
+}
+
+
+class DiscardServer(val port: Int) {
+
+    fun run() {
+        val bossGroup = NioEventLoopGroup()
+        val workerGroup = NioEventLoopGroup()
+        try {
+            val bootStrap = ServerBootstrap()
+            bootStrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel::class.java)
+                    .childHandler(object : ChannelInitializer<SocketChannel>(){
+                        override fun initChannel(ch: SocketChannel?) {
+                            ch?.pipeline()?.addLast(DiscardServerHandler())
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+            val future = bootStrap.bind(port).sync()
+            future.channel().closeFuture().sync()
+        } finally {
+            workerGroup.shutdownGracefully()
+            bossGroup.shutdownGracefully()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val port = if(args.isEmpty()) 12345 else args[0].toInt()
+    DiscardServer(port).run()
 }
