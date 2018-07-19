175bfcbab99f29a21f258341bb59811c71bc39b7
DATBOI <alexsimons9999@gmail.com>
2018-07-19 17:53:44 -0500

    oops!

--- src/main/kotlin/io/acari/springwebflux/Discard.kt
+++ src/main/kotlin/io/acari/springwebflux/Discard.kt
@@ -7,9 +7,8 @@ import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
-import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.util.ReferenceCountUtil
 import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioServerSocketChannel
 
 /**
  * Forged in the flames of battle by alex.
@@ -18,14 +17,8 @@ import io.netty.channel.socket.SocketChannel
 class DiscardServerHandler : ChannelInboundHandlerAdapter() {
     override fun channelRead(ctx: ChannelHandlerContext?, msg: Any?) {
         val message = msg as ByteBuf
-        try {
-            while (message.isReadable){
-                println(message.readByte().toChar())
-                System.out.flush()
-            }
-        } finally {
-            ReferenceCountUtil.release(msg)
-        }
+        ctx?.write(message)
+        ctx?.flush()
     }
 
     override fun exceptionCaught(ctx: ChannelHandlerContext?, cause: Throwable?) {
@@ -44,7 +37,7 @@ class DiscardServer(val port: Int) {
             val bootStrap = ServerBootstrap()
             bootStrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel::class.java)
-                    .childHandler(object : ChannelInitializer<SocketChannel>(){
+                    .childHandler(object : ChannelInitializer<SocketChannel>() {
                         override fun initChannel(ch: SocketChannel?) {
                             ch?.pipeline()?.addLast(DiscardServerHandler())
                         }
@@ -61,6 +54,6 @@ class DiscardServer(val port: Int) {
 }
 
 fun main(args: Array<String>) {
-    val port = if(args.isEmpty()) 12345 else args[0].toInt()
+    val port = if (args.isEmpty()) 12345 else args[0].toInt()
     DiscardServer(port).run()
 }
