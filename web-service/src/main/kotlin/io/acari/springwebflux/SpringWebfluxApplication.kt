20d1c490828294b68e48e20fe72e4a23053e459b
DATBOI <alexsimons9999@gmail.com>
2018-07-25 18:38:23 -0500

    Annotations for less/moar spring auto magic

--- web-service/src/main/kotlin/io/acari/springwebflux/SpringWebfluxApplication.kt
+++ web-service/src/main/kotlin/io/acari/springwebflux/SpringWebfluxApplication.kt
@@ -1,9 +1,14 @@
 package io.acari.springwebflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
-@SpringBootApplication
+@EnableReactiveMongoRepositories
+@SpringBootApplication(exclude = arrayOf(MongoAutoConfiguration::class,
+                                        MongoDataAutoConfiguration::class))
 class SpringWebfluxApplication
 
 fun main(args: Array<String>) {
