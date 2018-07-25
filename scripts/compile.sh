cfb15c7260ef831165db080d796b8a5939e0e2c0
DATBOI <alexsimons9999@gmail.com>
2018-07-25 18:25:26 -0500

    Less jetbrains stuff

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v /home/alex/workspace/mongo-images/web-content/:/app alexsimons/webflux-node-install:9.2.0 npm run compile
+docker run --rm -v /home/alex/workspace/mongo-images/web-content/:/app alexsimons/webflux-node-dev:9.2.0 npm run compile
