44d1107c06d941d133d9f9e59f3f3fb5b628649d
DATBOI <alexsimons9999@gmail.com>
2018-08-04 18:57:44 -0500

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

---
+++ web-content/src/app/pod/members/edit/PersonalInformationEditor.component.ts
@@ -0,0 +1,49 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Avatar} from '../model/Avatar.model';
+import {LocalAvatar} from '../model/LocalAvatar';
+import {TextPayload} from '../model/TextPayload';
+
+@Component({
+    selector: 'personal-information-editor',
+    template: require('./PersonalInformationEditor.component.htm')
+})
+export class PersonalInformationEditorComponent {
+
+    constructor() {
+    }
+
+    private _avatar: Avatar;
+
+    @Output()
+    private projectFileEmmiter = new EventEmitter<Avatar>();
+
+
+    @Input()
+    get avatar(): Avatar {
+        return this._avatar;
+    }
+
+    set avatar(value: Avatar) {
+        this._avatar = value;
+    }
+
+    //todo: remove dis when you can change remote projects.
+    get editMode(): boolean {
+        return this.avatar instanceof LocalAvatar;
+    }
+
+    updateFile(projectFile: Avatar): void {
+        this.avatar = projectFile;
+    }
+
+    get imageBinary(): Observable<any> {
+        return this._avatar.imageBinary();
+    }
+
+    private interests: TextPayload[] = [];
+
+    addInterest(textPayload: TextPayload){
+        this.interests.push(textPayload)
+    }
+}
