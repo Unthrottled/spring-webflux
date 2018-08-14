972f38f0fc4645e719791111ba9877c77dc85851
DATBOI <alexsimons9999@gmail.com>
2018-08-14 17:57:42 -0500

    also gunna need dis

---
+++ web-service/src/main/kotlin/io/acari/springwebflux/repository/PodMemberRepository.kt
@@ -0,0 +1,12 @@
+package io.acari.springwebflux.repository
+
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Service
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+@Service
+class PodMemberRepository(private val reactiveMongoTemplateDefined: ReactiveMongoTemplate){
+
+}
