c689f7f212389686fc272867aed21ab1b14ef540
Alex Simons <alexsimons9999@gmail.com>
2018-09-09 16:20:39 -0500

    Should help with confusion

--- web-service/src/main/kotlin/io/acari/springwebflux/rest/PreBuiltRouterComponent.kt
+++ web-service/src/main/kotlin/io/acari/springwebflux/rest/PreBuiltRouterComponent.kt
@@ -22,22 +22,23 @@ import org.springframework.web.reactive.function.server.ServerResponse
  * Forged in the flames of battle by alex.
  */
 @Component
-class RouterComponent(private val imageHandler: ImageHandler,
-                      private val podHandler: PodHandler) {
-
+class PreBuiltRouterComponent(private val imageHandler: ImageHandler,
+                              private val podHandler: PodHandler,
+                              private val preBuiltPodHandler: PreBuiltPodHandler) {
 
   @Bean
-  fun apiRouterFunction(): RouterFunction<ServerResponse> =
+  fun staticRouterFunction(): RouterFunction<*> =
+      resources("/**", ClassPathResource("static/"))
+
+  @Bean
+  fun alsoAPIRouterFunction(): RouterFunction<ServerResponse> =
       nest(path("/api"),
           nest(path("/pod"),
               route(GET("/members"), allPodMemberHandler())
-                  .andRoute(POST("/event"), podEventHandler())
-                  .andRoute(GET("/members/avatar"), allAvatarIdsHandler())
+                  .andRoute(GET("/event"), podGetEventHandler())
                   .andNest(path("/member/{id}"),
                       route(POST("/avatar"), saveImageHandler())
-                          .andRoute(GET("/avatar"), podMemberAvatarHandler())
-                          .andRoute(GET("/information"), podMemberInformationHandler())
-                          .andRoute(POST("/event"), podMemberEventHandler())
+                          .andRoute(GET("/event"), podMemberGetEventHandler())
                   ))
       )
 
@@ -47,35 +48,20 @@ class RouterComponent(private val imageHandler: ImageHandler,
         .body(fromPublisher(podHandler.projectAllPodMembers(), Identifier::class.java))
   }
 
-  private fun allAvatarIdsHandler() = HandlerFunction {
-    ServerResponse.ok()
-        .contentType(MediaType.APPLICATION_STREAM_JSON)
-        .body(fromPublisher(imageHandler.findAllNames(), Identifier::class.java))
-  }
-
-  private fun podMemberInformationHandler() = HandlerFunction {
-    ServerResponse.ok()
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(fromPublisher(podHandler.projectPersonalInformation(it.pathVariable("id")), PersonalInformation::class.java))
-  }
-
   private fun saveImageHandler() = HandlerFunction {
     ServerResponse.ok()
         .body(imageHandler.saveImage(it.body(BodyExtractors.toParts())), String::class.java)
   }
 
-  private fun podMemberEventHandler() = HandlerFunction {
+  private fun podMemberGetEventHandler() = HandlerFunction {
     ServerResponse.ok()
-        .body(podHandler.savePodMemberEvent(it.pathVariable("id"), it.bodyToMono(Event::class.java)), Event::class.java)
+        .contentType(MediaType.APPLICATION_STREAM_JSON)
+        .body(fromPublisher(preBuiltPodHandler.allPodMemberEvents(it.pathVariable("id")), Event::class.java))
   }
 
-  private fun podEventHandler() = HandlerFunction {
+  private fun podGetEventHandler() = HandlerFunction {
     ServerResponse.ok()
-        .body(podHandler.savePodEvent(it.bodyToMono(String::class.java)), String::class.java)
-  }
-
-  private fun podMemberAvatarHandler() = HandlerFunction {
-    ServerResponse.ok()
-        .body(podHandler.fetchAvatar(it.pathVariable("id")), ByteArray::class.java)
+        .contentType(MediaType.APPLICATION_STREAM_JSON)
+        .body(fromPublisher(preBuiltPodHandler.allPodEvents(), Event::class.java))
   }
 }
